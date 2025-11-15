4400be44338ce89bcf6cb1937317d14e7e0b565c
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2025-11-15 17:40:37 +0100

    Implemented a proper “refresh” path for the Blazor

--- PiggyzenMvp.Blazor/DTOs/Transactions/ManualCategorizeResponse.cs
+++ PiggyzenMvp.Blazor/DTOs/Transactions/ManualCategorizeResponse.cs
@@ -3,6 +3,7 @@ namespace PiggyzenMvp.Blazor.DTOs.Transactions;
 public class ManualCategorizeResponse
 {
     public int Categorized { get; set; }
+    public int AutoCategorized { get; set; }
     public List<ManualCategorizeError> Errors { get; set; } = new();
 }
 
