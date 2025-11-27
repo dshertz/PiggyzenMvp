7266f73bf922d95329aacef240527b5b9fbc8bcf
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2025-11-27 11:58:32 +0100

    Complete date filter in Blazor

--- PiggyzenMvp.Blazor/Services/TransactionFilterState.cs
+++ PiggyzenMvp.Blazor/Services/TransactionFilterState.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+
 namespace PiggyzenMvp.Blazor.Services;
+
+public enum DateFilterMode
+{
+    All,
+    Yearly,
+    Monthly
+}
 
 public class TransactionFilterState
 {
     public string SearchText { get; set; } = string.Empty;
     public string FilterCategory { get; set; } = "all";
     public bool IsLoadedFromStorage { get; set; }
+
+    public DateFilterMode DateMode { get; set; } = DateFilterMode.All;
+    public bool IsDateMultiSelect { get; set; }
+    public int? ActiveYear { get; set; }
+    public int? ActiveMonth { get; set; }
+    public List<int> SelectedYears { get; set; } = new();
+    public List<string> SelectedYearMonths { get; set; } = new();
 }
